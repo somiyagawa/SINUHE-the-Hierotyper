303bb0835d5df0edfbc6e9113ec2680c8acb7fb3
So Miyagawa <runa.uei@gmail.com>
2016-06-10 09:51:55 +0200

    Add files via upload

---
+++ renban.rb
@@ -0,0 +1,8 @@
+number = 2000000
+  File.open('besa.txt') do |file|
+  		file.each_line do |lab|    
+    		number += 1
+    	 	renban = number
+  	    	puts lab.gsub(/^/,"#{renban}\t")
+    	end
+  end
