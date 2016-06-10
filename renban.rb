c3a02113634fe38702220aff08cc893bddaff58e
So Miyagawa <runa.uei@gmail.com>
2016-06-10 09:52:37 +0200

    Update renban.rb

--- renban.rb
+++ renban.rb
@@ -1,8 +1,9 @@
 number = 2000000
-  File.open('besa.txt') do |file|
-  		file.each_line do |lab|    
-    		number += 1
-    	 	renban = number
-  	    	puts lab.gsub(/^/,"#{renban}\t")
-    	end
-  end
+
+File.open('besa.txt') do |file|
+  	file.each_line do |lab|    
+    	number += 1
+    	renban = number
+  	  puts lab.gsub(/^/,"#{renban}\t")
+    end
+end
