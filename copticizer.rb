322ca9b0fd65e184d2e60b141def23324653510d
So Miyagawa <runa.uei@gmail.com>
2016-06-10 10:03:58 +0200

    Update copticizer.rb

--- copticizer.rb
+++ copticizer.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 # Created by So Miyagawa, 2015
 # v 1.0
+# This is for Coptic NT of St. Shenouda Archimandrite Coptic Society
 print "What is the name of the file you want to copticize?"
 x=gets.chomp
 y=File.read("#{x}")
